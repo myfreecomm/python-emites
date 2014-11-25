c3523571db75f1a3ff9a30b7d4fdec3fa821c61e
Vitor M. A. da Cruz <vitor.mazzi@myfreecomm.com.br>
2014-11-25 15:28:59 -0200

    Version bump

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class Tox(TestCommand):
 
 setuptools.setup(
     name="python-emites",
-    version="0.0.5",
+    version="0.1.0",
     packages=["emites_api"],
     include_package_data=True,  # declarations in MANIFEST.in
     install_requires=open(join(dirname(__file__), 'requirements.txt')).readlines(),
